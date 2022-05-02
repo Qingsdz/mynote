e909f6b5c7b17668798a7446aaa7a3c21fed0d5a
Qingsdz <1839436665@qq.com>
2022-05-03 00:49:09 +0800

    2022-05-03 00:49:09

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
+#!/bin/zsh
 hugo -D
-br = `git branch | grep "*"`
+
+function obtain_git_branch {
+  br=`git branch | grep "*"`
+  echo ${br/* /}
+}
+result=`obtain_git_branch`
+
 git checkout autosubmit
 git add .
 git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git push origin autosubmit
-git checkout ${br}
+git checkout $result
